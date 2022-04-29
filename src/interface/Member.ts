08c23a6ae9551d4670cf2e9e5f5edf3b790c1e30
mohammadLam <md.nurul.islamlam2000@gmail.com>
2022-04-29 20:06:57 +0600

    typescript error fixed

--- src/interface/Member.ts
+++ src/interface/Member.ts
@@ -1,9 +1,9 @@
-import { genderType } from './type'
+import { GenderType } from './type'
 
 export default interface Member {
   name: string
   age: number
-  gender: genderType
+  gender: GenderType
   address: string
   phone: string
   password: string
